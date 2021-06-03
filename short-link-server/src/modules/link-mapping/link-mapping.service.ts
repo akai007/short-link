7711672b0a4dc23bc3380c23b7a458001a90b3a9
002002 <liaokaigogo@foxmail.com>
2021-06-03 19:35:44 +0800

    add short uuid logic

--- short-link-server/src/modules/link-mapping/link-mapping.service.ts
+++ short-link-server/src/modules/link-mapping/link-mapping.service.ts
@@ -3,6 +3,20 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { LinkMapping } from 'src/entity/link-mapping.entity';
 import { Repository } from 'typeorm';
 import { v5 as uuidv5 } from 'uuid';
+function getShortUUID(domain: string) {
+  const SHORT_ARRAY = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+  let uid = uuidv5(domain, uuidv5.URL);
+  uid = uid.replace(/-/g, '');
+  const buffer = [];
+  for (let i = 0; i < 8; i++) {
+    const start = i * 4;
+    const end = i * 4 + 4;
+    const str = uid.substring(start, end);
+    buffer.push(SHORT_ARRAY[parseInt(str, 16) % 64]);
+  }
+  return buffer.join('');
+}
+
 @Injectable()
 export class LinkMappingService {
   constructor(@InjectRepository(LinkMapping) private readonly linkMappingRepository: Repository<LinkMapping>) {}
@@ -13,17 +27,17 @@ export class LinkMappingService {
 
   async insert(linkMapping: LinkMapping): Promise<LinkMapping> {
     const insertObj = new LinkMapping();
+
+    // const rawUrlObj = new URL(linkMapping.rawUrl);
+    // const urlDomain = `${rawUrlObj.protocol}//:${rawUrlObj.host}`;
+    const compressCode = getShortUUID(linkMapping.rawUrl);
+    const DOMAIN = 'http://localhost';
+    const shortUrl = `${DOMAIN}/${compressCode}`;
+
     insertObj.rawUrl = linkMapping.rawUrl;
+    insertObj.shortUrl = shortUrl;
 
-    console.log(linkMapping, 'linkMapping', new URL(linkMapping.rawUrl));
-
-    const rawUrlObj = new URL(linkMapping.rawUrl);
-    const urlDomain = `${rawUrlObj.protocol}//:${rawUrlObj.host}`;
-    const compressCode = uuidv5(urlDomain, uuidv5.URL);
-    const shortUrl = `${urlDomain}/${compressCode}`;
-
-    console.log(shortUrl, rawUrlObj, urlDomain, compressCode);
-    return insertObj;
-    // return await this.linkMappingRepository.save(linkMapping);
+    // return insertObj;
+    return await this.linkMappingRepository.save(linkMapping);
   }
 }
